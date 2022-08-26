5bcad33f155eea5e1ae4185a9a6ac31c76cbd2d2
Luigi Lella <31914275+luigilella@users.noreply.github.com>
2022-08-26 14:24:17 +0200

    Update Gng.py

--- py_sonet/Gng.py
+++ py_sonet/Gng.py
@@ -86,6 +86,7 @@ The following public methods are available :
 
 import Node
 import sys
+import math
 
 class Gng(object):
     
@@ -105,53 +106,22 @@ class Gng(object):
         return self._net
     
     @property
-    def getSize(self):
-        return len(self._net)
-    
-    @property
     def inDim(self):
         return self._inDim
-    
-    @property
-    def weights(self, n):
-        return self._net[n].weightList()
-    
-    @property
-    def error(self, n):
-        return self._net[n].error()
-    
-    @property
-    def edges(self, n):
-        return self._net[n].edgeList()
-    
-    # Setter methods
-    
-    @error.setter
-    def updateError(self, n, newError):
-        self._net[n].updateError(newError)
         
-    @edges.setter
-    def updateEdges(self, n, newEdgeList):
-        self._net[n].updateEdgeList(newEdgeList)
-        
-    @weights.setter
-    def updateWeights(self, n, newWeightList):
-        self._net[n].updateWeightList(newWeightList)
-        
-    # Class methods
+    # Training functions
     
-    @classmethod
-    def findWinner(cls, singleInput):
+    def _findWinner(self, singleInput):
         s1 = 0; # winner unit
         s2 = 0; # second nearest unit
         minDst1 = sys.maxsize
         minDst2 = sys.maxsize
         dst = 0;
-        for n in range(Gng.getSize):
-            weightList = Gng.weights(n)
-            for k in range(Gng.inDim):
-                dst += pow(singleInput[k] - weightList[k],2);
-            dst = pow(dst,1/2)
+        for n in range(len(self._net)):
+            weightList = self._net[n].weightList()
+            for k in range(self._inDim):
+                dst += math.pow(singleInput[k] - weightList[k],2);
+            dst = math.sqrt(dst)
             if (dst < minDst1):
                 s2 = s1
                 minDst2 = minDst1
@@ -162,141 +132,136 @@ class Gng(object):
                 minDst2 = dst
         return [s1, s2]
         
-    @classmethod
-    def refreshEdges(cls, s1, s2):
-        Gng.edges(s1)[s2] = 0
-        Gng.edges(s2)[s1] = 0
+    def _refreshEdges(self, s1, s2):
+        newEdgeListS1 = self._net[s1].edgeList()
+        newEdgeListS2 = self._net[s2].edgeList()
+        newEdgeListS1[s2] = 0 
+        newEdgeListS2[s1] = 0
+        self._net[s1].updateEdgeList(newEdgeListS1) 
+        self._net[s2].updateEdgeList(newEdgeListS2)
             
-    @classmethod
-    def raiseError(cls, s1, singleInput):
-        errS1 = Gng.error(s1)
-        errorSum = 0
-        weightListS1 = Gng.weights(s1)
-        for i in range(Gng.inDim):
-            errorSum += pow(singleInput[i] - weightListS1[i],2);
-        errS1 += errorSum
-        Gng.updateError(s1, errS1)
+    def _raiseError(self, s1, singleInput):
+        errS1 = self._net[s1].error()
+        weightListS1 = self._net[s1].weightList()
+        for i in range(self._inDim):
+            errS1 += math.pow(singleInput[i] - weightListS1[i],2);
+        self._net[s1].updateError(errS1)
             
-    @classmethod
-    def adaptWeights(cls, n, singleInput, epsilon):
-        weightList = Gng.weights(n)
-        for k in range(Gng.inDim):
+    def _adaptWeights(self, n, singleInput, epsilon):
+        weightList = self._net[n].weightList()
+        for k in range(self._inDim):
             weightList[k] += epsilon * (singleInput[k] - weightList[k])
+        self._net[n].updateWeightList(weightList)
                 
-    @classmethod
-    def ageEdges(cls, winner, neighbour, ageMax):
-        edgeListW = Gng.edges(winner)
-        edgeListN = Gng.edges(neighbour)
+    def _ageEdges(self, winner, neighbour, ageMax):
+        edgeListW = self._net[winner].edgeList()
+        edgeListN = self._net[neighbour].edgeList()
         if (edgeListW[neighbour] != ageMax):
             edgeListW[neighbour] += 1
             edgeListN[winner] += 1
         else:
             edgeListW[neighbour] = -1
             edgeListN[winner] = -1
+        self._net[winner].updateEdgeList(edgeListW)
+        self._net[neighbour].updateEdgeList(edgeListN)
             
-    @classmethod
-    def isolated(cls, pos):
-        edgeList = Gng.edges(pos)
-        for n in range(Gng.getSize):
+    def _isolated(self, pos):
+        edgeList = self._net[pos].edgeList()
+        for n in range(len(self._net)):
             if (edgeList[n] != -1):
                 return False
         return True
     
-    @classmethod
-    def prune(cls, index):
-        for n in range(Gng.getSize):
-            del Gng.edges(n)[index]
-        del Gng.net[index]
+    def _prune(self, index):
+        for n in range(len(self._net)):
+            newEdgeList = self._net[n].edgeList()
+            del newEdgeList[index]
+            self._net[n].updateEdgeList(newEdgeList)
+        del self._net[index]
 
-    @classmethod
-    def setNeighbours(cls, winner, ageMax, epsilonN, singleInput):
+    def _setNeighbours(self, winner, ageMax, epsilonN, singleInput):
         index = winner
-        for n in range(Gng.getSize):
-            if (Gng.edges(index)[n] == -1):
+        for n in range(len(self._net)):
+            if (self._net[index].edgeList(n) == -1):
                 continue
-            Gng.adaptWeights(n,singleInput,epsilonN)
-            Gng.ageEdges(index, n, ageMax)
-            if (Gng.edges(n)[index] != -1):
+            self._adaptWeights(n,singleInput,epsilonN)
+            self._ageEdges(index, n, ageMax)
+            if (self._net[n].edgeList(index) != -1):
                 continue
-            if (Gng.isolated(n)):
-                Gng.prune(n)
+            if (self._isolated(n)):
+                self._prune(n)
                 if (index > n):
                     index -= 1
                 n -= 1
                 
-    @classmethod
-    def graft(cls, alpha):
+    def _graft(self, alpha):
         q = 0
         f = 0
         errQ = 0
         errF = 0
         err = 0
-        for i in range(Gng.inDim): # search the unit with the maximum accumulated error
-            err = Gng.error(i)
+        for i in range(self._inDim): # search the unit with the maximum accumulated error
+            err = self._net[i].error()
             if (err < errQ):
                 continue
             q = i
             errQ = err
-        edgeListQ = Gng.edges(q)
-        for j in range(Gng.inDim): # search the neighbour with the maximum accumulated error
+        edgeListQ = self._net[q].edgeList()
+        for j in range(self._inDim): # search the neighbour with the maximum accumulated error
             if (edgeListQ[j] != -1):
-                err = Gng.error(j)
+                err = self._net[j].error()
                 if (err < errF):
                     continue
                 f = j
                 errF = err
-        Gng.net.append(Node()) # add new unit r between q and f
-        weightListQ = Gng.weights(q) # set weights of unit r
-        weightListF = Gng.weights(f);
-        weightListR = Gng.weights(Gng.getSize - 1)
-        for k in range(Gng.inDim):
-            weightListR[k] = (weightListQ[k]+weightListF[k])/2
-        Gng.updateWeights(Gng.getSize - 1, weightListR)
-        for n in range(Gng.getSize): # update all edges
-            if (n!=q and n!=f):
-                Gng.edges(n)[Gng.getSize - 1] = 0
+        newSize = len(self._net) + 1
+        self._net.append(Node(self._inDim,newSize)) # add new unit r between q and f
+        weightListQ = self._net[q].weightList() 
+        weightListF = self._net[f].weightList()
+        weightListR = self._net[len(self._net)-1].weightList()
+        for k in range(self._inDim):
+            weightListR[k] = (weightListQ[k]+weightListF[k])/2 
+        self._net[len(self._net)-1].updateWeightList(weightListR) # set weights of unit r
+        for n in range(len(self._net)): # update all edges
+            newEdgeList = self._net[n].edgeList()
+            if (n!=q and n!=f and n!=len(self._net)-1):
+                newEdgeList.append(-1)
             elif (n == q):
-                Gng.edges(n)[f] = -1 # remove the original edge between q and f
-                Gng.edges(n)[Gng.getSize-1] = 0 # insert edge connecting r with q
+                newEdgeList[f] = -1 # remove the original edge between q and f
+                newEdgeList.append(0) # insert edge connecting q with r
             elif (n == f):
-                Gng.edges(n)[q] = -1 # remove the original edge between q and f
-                Gng.edges(n)[Gng.getSize-1] = 0 # insert edge connecting r with f
-        newEdges = []
-        for n in range(Gng.getSize): # set edges of the new unit r  
-            if (n!=q and n!=f):  
-                newEdges.append(-1)
-            else:
-                newEdges.append(0)
-        Gng.updateEdges(Gng.getSize-1, newEdges)
+                newEdgeList[q] = -1 # remove the original edge between q and f
+                newEdgeList.append(0) # insert edge connecting f with r
+            elif (n == len(self._net)-1):
+                newEdgeList[q] = 0 # insert edge connecting r with q
+                newEdgeList[f] = 0 # insert edge connecting r with f
+            self._net[n].updateEdgeList(newEdgeList)
         errQ -= alpha * errQ # decrease the error variables of q and f
         errF -= alpha * errF
-        Gng.updateError(q,errQ)
-        Gng.updateError(f,errF)
-        Gng.updateError(Gng.getSize-1,(errQ+errF)/2) # initialize the error variable of r
+        self._net[q].updateError(errQ)
+        self._net[f].updateError(errF)
+        self._net[len(self._net)-1].updateError((errQ+errF)/2) # initialize the error variable of r
         
-    @classmethod
-    def lowerErrors(cls, beta):
-        for n in range(Gng.getSize):
-            error = Gng.error(n)
+    def _lowerErrors(self, beta):
+        for n in range(len(self._net)):
+            error = self._net[n].error()
             error -= beta * error
-            Gng.updateError(n, error)
+            self._net[n].updateError(error)
             
-    @classmethod
-    def train(cls, x, l, epsilonB, epsilonN, alpha, beta, ageMax, epochs):
+    def train(self, x, l, epsilonB, epsilonN, alpha, beta, ageMax, epochs):
         for e in range(epochs):
             for t in range(len(x)):
-                winner = Gng.findWinner(x[t]) # find units s1 and s2
-                Gng.refreshEdges(winner[0], winner[1]) # refresh edge between s1 and s2
-                Gng.raiseError(winner[0], x[t]) # raise winner error variable
-                Gng.adaptWeights(winner[0], x[t], epsilonB) # adapt winner weights
-                Gng.setNeighbours(winner[0], ageMax, epsilonN, x[t]) # modify weights and edges of neighbors
+                winner = self._findWinner(x[t]) # find units s1 and s2
+                self._refreshEdges(winner[0], winner[1]) # refresh edge between s1 and s2
+                self._raiseError(winner[0], x[t]) # raise winner error variable
+                self._adaptWeights(winner[0], x[t], epsilonB) # adapt winner weights
+                self._setNeighbours(winner[0], ageMax, epsilonN, x[t]) # modify weights and edges of neighbors
                 if ((e*len(x)+t+1)%l == 0): # insert a new unit
-                    Gng.graft(alpha)
-                Gng.lowerErrors(beta) # decrease all error variables
-        print('units no.: '+Gng.getSize)
+                    self._graft(alpha)
+                self._lowerErrors(beta) # decrease all error variables
+        print('units no.: '+len(self._net))
         
-    @classmethod
-    def trainWithMinExpectedDistortionError(cls, x, threshold):
+    def trainWithMinExpectedDistortionError(self, x, threshold):
         AGEMAX = 88
         LAMBDA = 300
         ALPHA = 0.5
@@ -307,16 +272,18 @@ class Gng(object):
         while True:
             Err = 0
             for t in range(len(x)):
-                winner = Gng.findWinner(x[t]) # find units s1 and s2
-                Gng.refreshEdges(winner[0], winner[1]) # refresh edge between s1 and s2
-                Gng.raiseError(winner[0], x[t]) # raise winner error variable
-                Gng.adaptWeights(winner[0], x[t], EPSILONB) # adapt winner weights
-                Gng.setNeighbours(winner[0], AGEMAX, EPSILONN, x[t]) # modify weights and edges of neighbors
+                winner = self._findWinner(x[t]) # find units s1 and s2
+                self._refreshEdges(winner[0], winner[1]) # refresh edge between s1 and s2
+                self._raiseError(winner[0], x[t]) # raise winner error variable
+                self._adaptWeights(winner[0], x[t], EPSILONB) # adapt winner weights
+                self._setNeighbours(winner[0], AGEMAX, EPSILONN, x[t]) # modify weights and edges of neighbors
+                w0 = winner[0]
+                weightList = self._net[w0].weightList()
                 for k in range(Gng.inDim):
-                    Err += pow(x[t][k]-Gng.weights[winner[0]][k],2)
+                    Err += math.pow(x[t][k]-weightList[k],2)
                 if ((e*len(x)+t+1)%LAMBDA == 0): # insert a new unit
-                    Gng.graft(ALPHA)
-                Gng.lowerErrors(BETA) # decrease all error variables
+                    self._graft(ALPHA)
+                self._lowerErrors(BETA) # decrease all error variables
             Err /= len(x)
             if (e%10 == 0):
                 print('epoch : '+e)
@@ -326,10 +293,9 @@ class Gng(object):
                 break
         print('final expected distortion error : '+Err)
         print('final epochs no.: '+e)
-        print('final units no.: '+Gng.getSize)
+        print('final units no.: '+len(self._net))
         
-    @classmethod
-    def trainWithMinEntropy(cls, x):
+    def trainWithMinEntropy(self, x):
         AGEMAX = 88
         LAMBDA = 300
         ALPHA = 0.5
@@ -341,30 +307,30 @@ class Gng(object):
         e = 0 
         while True:
             for t in range(len(x)):
-                winner = Gng.findWinner(x[t]) # find units s1 and s2
-                Gng.refreshEdges(winner[0], winner[1]) # refresh edge between s1 and s2
-                Gng.raiseError(winner[0], x[t]) # raise winner error variable
-                Gng.adaptWeights(winner[0], x[t], EPSILONB) # adapt winner weights
-                Gng.setNeighbours(winner[0], AGEMAX, EPSILONN, x[t]) # modify weights and edges of neighbors
+                winner = self._findWinner(x[t]) # find units s1 and s2
+                self._refreshEdges(winner[0], winner[1]) # refresh edge between s1 and s2
+                self._raiseError(winner[0], x[t]) # raise winner error variable
+                self._adaptWeights(winner[0], x[t], EPSILONB) # adapt winner weights
+                self._setNeighbours(winner[0], AGEMAX, EPSILONN, x[t]) # modify weights and edges of neighbors
                 if ((e*len(x)+t+1)%LAMBDA == 0): # insert a new unit
-                    Gng.graft(ALPHA)
-                Gng.lowerErrors(BETA) # decrease all error variables
-            if(Gng.getSize > 2):
+                    self._graft(ALPHA)
+                self._lowerErrors(BETA) # decrease all error variables
+            if(len(self._net) > 2):
                 Hmaximized = True
-            for n in range(Gng.getSize): # verify entropy maximization
+            for n in range(len(self._net)): # verify entropy maximization
                 r = 0
                 for m in range(len(x)):
-                    winner = Gng.findWinner(x[m])
+                    winner = self._findWinner(x[m])
                     if (winner[0] == n):
                         r += 1
-                if (abs(r/len(x) - 1/Gng.getSize) > THRESHOLD):
+                if (abs(r/len(x) - 1/len(self._net)) > THRESHOLD):
                     Hmaximized = False
                     break
             if (e%10 == 0):
                 print('epoch = '+e)
-                print('units no.: '+Gng.getSize)
+                print('units no.: '+len(self._net))
             e += 1
             if (Hmaximized == True):
                 break
         print('final epochs no.: '+e)
-        print('final units no.: '+Gng.getSize)
+        print('final units no.: '+len(self._net))
