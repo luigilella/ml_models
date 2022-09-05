817cbabc5c3e86aebe9a71ef5243e795e1b7c215
Luigi Lella <31914275+luigilella@users.noreply.github.com>
2022-09-05 09:45:24 +0200

    Create Population.java

---
+++ swarmevolutive/Population.java
@@ -0,0 +1,81 @@
+/****************************************************************************
+
+Copyright (C) (2022) Luigi Lella
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either  version 3 of the License, or  (at your option)  any later
+version.
+
+This program is distributed  in the hope that it will be useful,  but WITHOUT 
+ANY WARRANTY, without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License  along with 
+this program. If not, see <http://www.gnu.org/licenses/>.
+
+*****************************************************************************/
+
+package swarmevolutive;
+
+public class Population {
+
+    Individual[] individuals;
+
+    /*
+     * Constructors
+     */
+    // Create a population
+    public Population(int populationSize, boolean initialise) {
+        individuals = new Individual[populationSize];
+        // Initialise population
+        if (initialise) {
+            // Loop and create individuals
+            for (int i = 0; i < size(); i++) {
+                Individual newIndividual = new Individual();
+                newIndividual.generateIndividual();
+                saveIndividual(i, newIndividual);
+            }
+        }
+    }
+
+    /* Getters */
+    public Individual getIndividual(int index) {
+        return individuals[index];
+    }
+    
+    public Individual getFittest() {
+        Individual fittest = individuals[0];
+        // Loop through individuals to find fittest
+        for (int i = 0; i < size(); i++) {
+            if (fittest.getFitness() <= getIndividual(i).getFitness()) {
+                fittest = getIndividual(i);
+            }
+        }
+        return fittest;
+    }
+    
+    public Individual getSecondFittest() {
+    	Individual fittest = this.getFittest();
+        Individual secondFittest = individuals[0];
+        // Loop through individuals to find fittest
+        for (int i = 0; i < size(); i++) {
+            if ((secondFittest.getFitness() <= getIndividual(i).getFitness())&&(!getIndividual(i).equals(fittest))) 
+            {
+            	secondFittest = getIndividual(i);
+            }
+        }
+        return secondFittest;
+    }
+
+    /* Public methods */
+    // Get population size
+    public int size() {
+        return individuals.length;
+    }
+
+    // Save individual
+    public void saveIndividual(int index, Individual indiv) {
+        individuals[index] = indiv;
+    }
+}
